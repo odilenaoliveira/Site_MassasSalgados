20bcff5d130b4ee6728581dc13a1c2f090e27b3b
Odilena Oliveira <91203512+odilenaoliveira@users.noreply.github.com>
2024-07-18 12:56:37 +0100

    esboco

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+window.addEventListener("scroll",function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY> 0);
+});
+
+//Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
